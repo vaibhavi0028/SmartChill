a29942babbfbf5c85dd982d601e716caaad69228
Vaibhavi <vaibhavi.2022a@vitstudent.ac.in>
2024-09-28 05:06:44 +0530

    updated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,13 +3,18 @@ import DashboardStatsGrid from "../components/DashboardStatsGrid";
 import Cards from "../components/Cards";
 
 export default function Dashboard({ scrollToChiller, scrollToFault }) {
+	const cardsRef = useRef(null);
+	const scrollToCards = () => {
+		if (cardsRef.current) {
+			cardsRef.current.scrollIntoView({ behavior: 'smooth' });
+		}
+	};
 	return (
 	  <div className="flex flex-col gap-4">
-		<DashboardStatsGrid />
-		<div className="flex flex-row gap-4 w-full">
+		<DashboardStatsGrid onButtonClick={scrollToCards}  />
+		<div className="flex flex-row gap-4 w-full" ref={cardsRef}>
 		  <Cards scrollToChiller={scrollToChiller} scrollToFault={scrollToFault} />
 		</div>
 	  </div>
 	);
   }
-  
